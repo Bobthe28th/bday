a48a5e80616802676ac9fb1aa579a04f26701917
Bobthe28th <owenmjones28@gmail.com>
2023-10-12 14:34:53 -0700

    Actually makes the plugin compile, also scp

--- src/main/java/me/bobthe28th/ctf/Main.java
+++ src/main/java/me/bobthe28th/ctf/Main.java
@@ -1,5 +1,6 @@
 package me.bobthe28th.ctf;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,6 +9,7 @@ public class Main extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this,this);
+        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "This is a test!");
     }
 
 }
